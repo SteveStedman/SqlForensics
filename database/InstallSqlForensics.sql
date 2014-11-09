73374c6c5c265c5c9ba9da915aafcea552790afd
SteveStedman <github@stedman.us>
2014-11-09 10:45:43 -0800

    working on updating the script

--- database/InstallSqlForensics.sql
+++ database/InstallSqlForensics.sql
@@ -4,12 +4,16 @@ SET NOCOUNT ON;
 
 IF EXISTS(SELECT name FROM sys.databases WHERE name = 'SqlForensics')
 BEGIN
-	--RAISERROR ('Database SqlForensics Already Exists', 20, 1)  WITH LOG
-	ALTER DATABASE [SqlForensics] 
-	  SET SINGLE_USER WITH ROLLBACK IMMEDIATE;
-	DROP DATABASE [SqlForensics];
+	RAISERROR ('Database SqlForensics Already Exists', 20, 1)  WITH LOG
+	-- comment out the RAISEERROR line above and uncomment the following three lines if you
+	--   are running the script a second time. 
+	   
+	--ALTER DATABASE [SqlForensics] 
+	--  SET SINGLE_USER WITH ROLLBACK IMMEDIATE;
+	--DROP DATABASE [SqlForensics];
 END
-CREATE DATABASE [SqlForensics];
+CREATE DATABASE [SqlForensics]
+COLLATE SQL_Latin1_General_CP1_CS_AS;
 GO
 
 USE [SqlForensics];
@@ -23,26 +27,63 @@ CREATE TABLE [ForensicLogging].[Configuration] (
     [value] VARCHAR (5000)
 	);
 
-CREATE TABLE [ForensicLogging].[Log] (
-	[id] BIGINT IDENTITY(-9223372036854775808, 1),  
-    [whenRecorded] DATETIME2 DEFAULT SYSDATETIME(),
-	[typeId] SMALLINT NOT NULL,
-    [itemId] BIGINT NOT NULL,
-    [value] VARCHAR (MAX)
-	);
+GO
+CREATE TABLE [ForensicLogging].[LogTypes](
+	[id] [smallint] IDENTITY(-32768,1) NOT NULL,
+	[type] [varchar](50) NULL,
+ CONSTRAINT [PK_LogTypes] PRIMARY KEY CLUSTERED 
+(
+	[id] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
+) ON [PRIMARY]
 
-CREATE TABLE [ForensicLogging].[LogTypes] (
-	[id] SMALLINT IDENTITY(-32768, 1),  
-	[type] VARCHAR (50)
-	);
-
-CREATE TABLE [ForensicLogging].[LogItems] (
-	[id] BIGINT IDENTITY(-9223372036854775808, 1),
-	[theItemsId] INTEGER NOT NULL,
-    [name] VARCHAR (5000),
-	);
 GO
 
+
+
+CREATE TABLE [ForensicLogging].[LogItems](
+	[id] [bigint] IDENTITY(-9223372036854775808,1) NOT NULL,
+	[theItemsId] [int] NOT NULL,
+	[name] [varchar](5000) NULL,
+ CONSTRAINT [PK_LogItems] PRIMARY KEY CLUSTERED 
+(
+	[id] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
+) ON [PRIMARY]
+GO
+
+CREATE TABLE [ForensicLogging].[Log](
+	[id] [bigint] IDENTITY(-9223372036854775808,1) NOT NULL,
+	[whenRecorded] [datetime2](7) NULL,
+	[typeId] [smallint] NOT NULL,
+	[itemId] [bigint] NOT NULL,
+	[value] [varchar](max) NULL,
+ CONSTRAINT [PK_Log] PRIMARY KEY CLUSTERED 
+(
+	[id] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
+) ON [PRIMARY] TEXTIMAGE_ON [PRIMARY];
+GO
+
+ALTER TABLE [ForensicLogging].[Log] ADD  DEFAULT (sysdatetime()) FOR [whenRecorded];
+GO
+
+ALTER TABLE [ForensicLogging].[Log]  WITH CHECK ADD  CONSTRAINT [FK_Log_LogItems] FOREIGN KEY([itemId])
+REFERENCES [ForensicLogging].[LogItems] ([id]);
+GO
+
+ALTER TABLE [ForensicLogging].[Log] CHECK CONSTRAINT [FK_Log_LogItems];
+GO
+
+ALTER TABLE [ForensicLogging].[Log]  WITH CHECK ADD  CONSTRAINT [FK_Log_LogTypes] FOREIGN KEY([typeId])
+REFERENCES [ForensicLogging].[LogTypes] ([id]);
+GO
+
+ALTER TABLE [ForensicLogging].[Log] CHECK CONSTRAINT [FK_Log_LogTypes];
+
+
+
+GO
 CREATE PROCEDURE [ForensicLogging].[getLogTypeId]
 	@type VARCHAR (50)
 AS
@@ -79,7 +120,8 @@ BEGIN
 	BEGIN
 		MERGE [ForensicLogging].[LogItems]  AS target       
 		USING (SELECT configuration_id, name  FROM sys.configurations as c) AS source (theItemsId, name)       
-		   ON (target.theItemsId = source.theItemsId and target.name = source.name)   
+		   ON (target.theItemsId = source.theItemsId AND 
+			   target.name COLLATE DATABASE_DEFAULT = source.name COLLATE DATABASE_DEFAULT)   
 		 WHEN NOT MATCHED THEN                                 
 	   INSERT (theItemsId, name)                                 
 	   VALUES (source.theItemsId, source.name);  
@@ -87,7 +129,7 @@ BEGIN
 		;WITH wrapperCTE AS
 		( 
 			SELECT [itemId], [value], 
-				   ROW_NUMBER() OVER (PARTITION BY [typeId], [itemId] ORDER BY ID DESC) as sorter
+				   ROW_NUMBER() OVER (PARTITION BY [typeId], [itemId] ORDER BY [id] DESC) as sorter
 			  FROM [ForensicLogging].[Log]
 			 WHERE [typeId] = @logTypeId
 		),
@@ -99,9 +141,12 @@ BEGIN
 		)
 		,currentCTE	AS
 		(
-			SELECT l.id as itemId, cast(c.VALUE AS VARCHAR(100)) + '/' + cast(c.VALUE_IN_USE as VARCHAR(100)) as VALUE 
+			SELECT l.id as itemId, 
+			       cast(c.VALUE AS VARCHAR(100)) + '/' + cast(c.VALUE_IN_USE as VARCHAR(100)) as value
 			  FROM sys.configurations as c
-			 INNER JOIN [ForensicLogging].[LogItems] as l ON l.theItemsId = c.CONFIGURATION_ID AND l.name = c.NAME
+			 INNER JOIN [ForensicLogging].[LogItems] as l 
+			         ON l.theItemsId = c.CONFIGURATION_ID AND 
+					    l.name COLLATE DATABASE_DEFAULT = c.NAME COLLATE DATABASE_DEFAULT 
 		)
 		INSERT INTO [ForensicLogging].[Log] ([typeId], [itemId], [value]) 
 		SELECT @logTypeId, c.*
